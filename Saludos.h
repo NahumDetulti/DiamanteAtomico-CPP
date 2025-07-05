eae63eb94711a99c48f5d47a89e18e0b94fa47e9
NahumDevDetulti <nahumsanchezarce7@gmail.com>
2025-07-05 09:30:51 -0600

    Saludos.h

---
+++ Saludos.h
@@ -0,0 +1,11 @@
+```
+#ifndef SALUDOS_H
+#define SALUDOS_H
+#include <string>
+class Saludos {
+public:
+    std::string obtenerSaludo();
+    std::string obtenerHoraDelDia();
+};
+#endif // SALUDOS_H
+```
